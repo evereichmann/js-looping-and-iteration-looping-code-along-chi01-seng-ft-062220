1bf89a659b6682f75472871860099108d6792b62
Eve Reichmann <eve.reichmann@gmail.com>
2020-08-03 18:43:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( namesarray, event ) {
+    let thankcards = []
+    for ( let i = 0; i < namesarray.length; i++ ) {
+      thankcards.push( `Thank you, ${namesarray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankcards
+  }
+  
+  function countDown( num ) {
+    while ( num > 0 ) {
+      console.log( num );
+      num -= 1;
+    }
+    console.log( num );
+  }
